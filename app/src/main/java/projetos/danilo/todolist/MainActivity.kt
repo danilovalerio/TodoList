77cd4e7ef165ddc84a6ed222b5456f9eeb4cca62
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-06-24 17:15:51 -0300

    recycler and binding Main

--- app/src/main/java/projetos/danilo/todolist/MainActivity.kt
+++ app/src/main/java/projetos/danilo/todolist/MainActivity.kt
@@ -1,11 +1,29 @@
 package projetos.danilo.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import projetos.danilo.todolist.databinding.ActivityAddTaskBinding
+import projetos.danilo.todolist.databinding.ActivityMainBinding
+import projetos.danilo.todolist.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupListeners()
+
+
+    }
+
+    private fun setupListeners() {
+        binding.fabAdd.setOnClickListener{
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
     }
 }
