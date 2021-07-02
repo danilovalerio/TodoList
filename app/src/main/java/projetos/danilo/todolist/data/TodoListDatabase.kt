d4b1ce46f4d079a33c0468ae531499a115d3249a
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-02 10:28:58 -0300

    todo list database com a exposicao para outras threads atraves do volatile

--- app/src/main/java/projetos/danilo/todolist/data/TodoListDatabase.kt
+++ app/src/main/java/projetos/danilo/todolist/data/TodoListDatabase.kt
@@ -1,8 +1,32 @@
 package projetos.danilo.todolist.data
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 abstract class TodoListDatabase: RoomDatabase() {
 
-    abstract fun todoListDao():
+    abstract fun todoListDao(): TodoListDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TodoListDatabase? = null
+
+        fun getDatabase(ctx: Context): TodoListDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    ctx.applicationContext,
+                    TodoListDatabase::class.java,
+                    "todolist_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
