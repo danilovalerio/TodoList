7bf22ec1b161813f56bbb7b297d809a3f4f186ba
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-07-09 12:46:24 -0300

    no action bar

--- app/src/main/java/projetos/danilo/todolist/ui/SplashActivity.kt
+++ app/src/main/java/projetos/danilo/todolist/ui/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
 import projetos.danilo.todolist.R
 
 class SplashActivity : AppCompatActivity() {
@@ -12,7 +13,13 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         supportActionBar?.hide()
+
+        //TODO: Add animation in logo tasks image view
 
         Handler(Looper.getMainLooper()).postDelayed({openMainActivity()}, 1500)
     }
